905c18c899559cd828b6ce3c71361069889a13ab
Patrick Kurmann <kurmannwillisau@me.com>
2018-05-22 22:20:58 +0200

    Small cosmetic changes

--- expeditionsjournal-web/src/app/mock-expedition-list.ts
+++ expeditionsjournal-web/src/app/mock-expedition-list.ts
@@ -3,5 +3,6 @@ import { Expedition } from './expedition';
 export const EXPEDITIONLIST: Expedition[] = [
     { id: 1, name: 'Moléson', altitude_meters: 2002 },
     { id: 2, name: 'Ulmizberg', altitude_meters: 937 },
-    { id: 3, name: 'Gurten', altitude_meters: 858}
+    { id: 3, name: 'Parrotspitze', altitude_meters: 4434 },
+    { id: 4, name: 'Gurten', altitude_meters: 858 }
 ];
