8208b860bf2b42d13456973bf73e6b22a96de721
Patrick Kurmann <patrick.kurmann.1@post.ch>
2018-05-23 16:16:20 +0200

    Expedition list with comment property inluding sample data

--- expeditionsjournal-web/src/app/expedition.ts
+++ expeditionsjournal-web/src/app/expedition.ts
@@ -2,4 +2,5 @@ export class Expedition {
     id: number;
     name: string;
     altitude_meters: number;
+    comment?: string;
 }
--- expeditionsjournal-web/src/app/mock-expedition-list.ts
+++ expeditionsjournal-web/src/app/mock-expedition-list.ts
@@ -1,8 +1,8 @@
 import { Expedition } from './expedition';
 
 export const EXPEDITIONLIST: Expedition[] = [
-    { id: 1, name: 'Moléson', altitude_meters: 2002 },
+    { id: 1, name: 'Moléson', altitude_meters: 2002, comment: 'Bestiegen in drei Stunden ab Gruyière Dorf' },
     { id: 2, name: 'Ulmizberg', altitude_meters: 937 },
-    { id: 3, name: 'Parrotspitze', altitude_meters: 4434 },
-    { id: 4, name: 'Gurten', altitude_meters: 858 }
+    { id: 3, name: 'Parrotspitze', altitude_meters: 4434, comment: 'Bisher höchster erreichter Gipfel' },
+    { id: 4, name: 'Gurten', altitude_meters: 858, comment: 'Der erste Gipfel nach der abgegebenen Masterarbeit' }
 ];
