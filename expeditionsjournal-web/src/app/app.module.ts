3ac6bcb22ce1f24048cecc3901a4febd2a3f644b
Patrick Kurmann <patrick.kurmann.1@post.ch>
2018-05-23 15:47:12 +0200

    Defined header as own component

--- expeditionsjournal-web/src/app/app.module.ts
+++ expeditionsjournal-web/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ExpeditionListComponent } from './expedition-list/expedition-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpeditionListComponent
-  ],
+    ExpeditionListComponent,
+    HeaderComponent
+],
   imports: [
     BrowserModule,
     FormsModule
