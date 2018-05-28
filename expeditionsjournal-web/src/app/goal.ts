d955bcfe38a4ce0b3db284fb45b8618c06bb791b
Patrick Kurmann <patrick.kurmann@post.ch>
2018-05-28 15:26:16 +0200

    Extended Goal by current amount

--- expeditionsjournal-web/src/app/goal.ts
+++ expeditionsjournal-web/src/app/goal.ts
@@ -3,6 +3,7 @@ import { Period } from './period.enum';
 export class Goal {
     id: string;
     targetAmount: number;
+    currentAmount?: number;
     name: string;
     description?: string;
     period: Period;
