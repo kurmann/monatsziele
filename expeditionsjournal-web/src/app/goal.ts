5142dc5ea447b1819e496665e4900b071f32a4f0
Patrick Kurmann <patrick.kurmann@post.ch>
2018-05-28 11:14:15 +0200

    Created goal and period enumeration as the base for the new goal app

---
+++ expeditionsjournal-web/src/app/goal.ts
@@ -0,0 +1,8 @@
+import { Period } from './period.enum';
+
+export class Goal {
+    id: string;
+    name: string;
+    description: string;
+    period: Period;
+}
---
+++ expeditionsjournal-web/src/app/period.enum.ts
@@ -0,0 +1,6 @@
+export enum Period {
+    Week,
+    Month,
+    Quarter,
+    Year
+}
