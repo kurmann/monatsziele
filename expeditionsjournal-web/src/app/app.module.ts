3687cec1f359f65fd51c803af1d1af4a6f374f1b
Patrick Kurmann <kurmannwillisau@me.com>
2018-05-28 21:33:51 +0200

    Removed non-working edit component

--- expeditionsjournal-web/src/app/app.module.ts
+++ expeditionsjournal-web/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
 import { GoalListComponent } from './goal-list/goal-list.component';
 import { GoalDetailComponent } from './goal-detail/goal-detail.component';
-import { GoalEditComponent } from './goal-edit/goal-edit.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +15,6 @@ import { GoalEditComponent } from './goal-edit/goal-edit.component';
     AboutComponent,
     GoalListComponent,
     GoalDetailComponent,
-    GoalEditComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +22,6 @@ import { GoalEditComponent } from './goal-edit/goal-edit.component';
     RouterModule.forRoot([
       { path: 'goals', component: GoalListComponent },
       { path: 'goals/:id/detail', component: GoalDetailComponent },
-      { path: 'goals/:id/edit', component: GoalEditComponent, },
       { path: 'goals/:id',  redirectTo: 'goals/:id/detail', pathMatch: 'full' },
       { path: 'about', component: AboutComponent },
       { path: 'home', redirectTo: 'goals', pathMatch: 'full' },
