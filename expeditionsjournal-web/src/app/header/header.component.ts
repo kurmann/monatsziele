59e81e3ca1275637486134ba1350012ef2358dc6
Patrick Kurmann <patrick.kurmann@post.ch>
2018-05-28 12:46:36 +0200

    Umbenennung in Jahresziele

--- expeditionsjournal-web/src/app/header/header.component.ts
+++ expeditionsjournal-web/src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title = 'Expeditionsjournal';
+  title = 'Jahresziele';
 
   constructor() { }
 
