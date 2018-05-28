af6eaf0719299cbffa45fb13f25488cb2d75a1f8
Patrick Kurmann <patrick.kurmann@post.ch>
2018-05-28 11:53:58 +0200

    Added targetAmount as property

--- expeditionsjournal-web/src/app/goal.ts
+++ expeditionsjournal-web/src/app/goal.ts
@@ -2,7 +2,8 @@ import { Period } from './period.enum';
 
 export class Goal {
     id: string;
+    targetAmount: number;
     name: string;
-    description: string;
+    description?: string;
     period: Period;
 }
