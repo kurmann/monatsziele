beacd5cab9569a6310c2a81db247ee249837b249
Patrick Kurmann <kurmannwillisau@me.com>
2018-05-29 14:05:42 +0200

    Getter and setter for current amount

--- expeditionsjournal-web/src/app/goal.ts
+++ expeditionsjournal-web/src/app/goal.ts
@@ -1,11 +1,44 @@
 import { Period } from './period.enum';
 
 export class Goal {
-    readonly id: string;
-    targetAmount: number;
-    currentAmount?: number;
+    id: string;
     name: string;
     description?: string;
     period: Period;
     currentPercentage?: number;
+
+    private _targetAmount: number;
+    get targetAmount(): number {
+        return this._targetAmount;
+    }
+    set targetAmount(newTargetAmount: number) {
+
+        // set value
+        this._targetAmount = newTargetAmount;
+
+        // update current percentage
+        if (newTargetAmount === undefined || newTargetAmount === NaN || newTargetAmount === 0) {
+            this.currentPercentage = 0;
+        } else {
+            this.currentPercentage = this.currentAmount / this.targetAmount;
+        }
+    }
+
+    private _currentAmount: number;
+    get currentAmount(): number {
+        return this._currentAmount;
+    }
+    set currentAmount(newCurrentAmount: number) {
+
+        // set value
+        this._currentAmount = newCurrentAmount;
+
+        // update current percentage
+        if (newCurrentAmount === undefined || newCurrentAmount === NaN) {
+            this.currentPercentage = 0;
+        } else {
+            this.currentPercentage = this.currentAmount / this.targetAmount;
+            console.log('New current percentage: ' + this.currentPercentage);
+        }
+    }
 }
