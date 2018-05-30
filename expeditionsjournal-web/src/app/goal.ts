390d7d7c43f933418278130d7770bb12508480e7
Patrick Kurmann <kurmannwillisau@me.com>
2018-05-30 14:09:05 +0200

    Removed getter-setter on targetAmount and currentAmount (caused TS2322)

--- expeditionsjournal-web/src/app/goal.ts
+++ expeditionsjournal-web/src/app/goal.ts
@@ -5,40 +5,7 @@ export class Goal {
     name: string;
     description?: string;
     period: Period;
+    currentAmount: number;
+    targetAmount: number;
     currentPercentage?: number;
-
-    private _targetAmount: number;
-    get targetAmount(): number {
-        return this._targetAmount;
-    }
-    set targetAmount(newTargetAmount: number) {
-
-        // set value
-        this._targetAmount = newTargetAmount;
-
-        // update current percentage
-        if (newTargetAmount === undefined || newTargetAmount === NaN || newTargetAmount === 0) {
-            this.currentPercentage = 0;
-        } else {
-            this.currentPercentage = this.currentAmount / this.targetAmount;
-        }
-    }
-
-    private _currentAmount: number;
-    get currentAmount(): number {
-        return this._currentAmount;
-    }
-    set currentAmount(newCurrentAmount: number) {
-
-        // set value
-        this._currentAmount = newCurrentAmount;
-
-        // update current percentage
-        if (newCurrentAmount === undefined || newCurrentAmount === NaN) {
-            this.currentPercentage = 0;
-        } else {
-            this.currentPercentage = this.currentAmount / this.targetAmount;
-            console.log('New current percentage: ' + this.currentPercentage);
-        }
-    }
 }
