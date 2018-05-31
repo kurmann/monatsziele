b54f61b43f0f7f09d49203c4ae9a294fc784fdb1
Patrick Kurmann <patrick.kurmann@post.ch>
2018-05-31 08:48:45 +0200

    Changed header to Monatsziele and app title

--- expeditionsjournal-web/src/app/header/header.component.ts
+++ expeditionsjournal-web/src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  title = 'Jahresziele';
+  title = 'Monatsziele';
 
   constructor() { }
 
