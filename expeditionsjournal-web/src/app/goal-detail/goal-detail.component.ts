cecf9804947d647914a0921ba65b16f0f2155278
Patrick Kurmann <kurmannwillisau@me.com>
2018-06-03 14:11:17 +0200

    Removed goal list test data of recordings #12

--- expeditionsjournal-web/src/app/goal-detail/goal-detail.component.ts
+++ expeditionsjournal-web/src/app/goal-detail/goal-detail.component.ts
@@ -11,19 +11,12 @@ import { Recording } from '../recording';
 export class GoalDetailComponent implements OnInit {
 
   public goal: Goal;
-  public recording: Recording;
 
   constructor(private route: ActivatedRoute, private router: Router, private goalService: GoalService) { }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.goal = this.goalService.getGoal(id);
-
-    // test
-    this.recording = new Recording ();
-    this.recording.date = new Date();
-    this.recording.goalIds = ['abacc95c-9515-4470-be53-cf3c15276796'];
-    this.recording.amount = 1;
   }
 
   onBack() {
