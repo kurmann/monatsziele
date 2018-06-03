441366e0d2ed674bcfad7be31913971e43f87d25
Patrick Kurmann <kurmannwillisau@me.com>
2018-06-03 15:11:35 +0200

    Displays recordings via component with recordings as input param #12

--- expeditionsjournal-web/src/app/recording-list/recording-list.component.ts
+++ expeditionsjournal-web/src/app/recording-list/recording-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recording } from '../recording';
 
 @Component({
   selector: 'app-recording-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recording-list.component.css']
 })
 export class RecordingListComponent implements OnInit {
+
+  @Input() recordings: Recording[];
 
   constructor() { }
 
