92690492dff76aadb154a2162ceb97d89f9f7b62
kurmann <kurmannwillisau@me.com>
2018-06-07 14:23:08 +0200

    Loading new empty recording on init #16

--- expeditionsjournal-web/src/app/goal-detail-create-recording/goal-detail-create-recording.component.ts
+++ expeditionsjournal-web/src/app/goal-detail-create-recording/goal-detail-create-recording.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GoalService } from '../goal-service/goal.service';
 import { Goal } from '../goal';
+import { Recording } from '../recording';
 
 @Component({
   templateUrl: './goal-detail-create-recording.component.html',
@@ -9,15 +10,23 @@ import { Goal } from '../goal';
 })
 export class GoalDetailCreateRecordingComponent implements OnInit {
 
-  _goal: Goal;
+  goal: Goal;
+  recording: Recording;
 
   constructor(private _route: ActivatedRoute, private _goalService: GoalService) { }
 
   ngOnInit() {
     const id = this._route.parent.snapshot.paramMap.get('id');
-    this._goal = this._goalService.getGoal(id);
+    this.goal = this._goalService.getGoal(id);
 
-    console.log('Goal:', this._goal.id);
+    this.recording = this.getNewRecording();
   }
 
+  private getNewRecording(): Recording {
+    const recording = new Recording();
+    recording.date = new Date();
+    recording.amount = 1;
+
+    return recording;
+  }
 }
