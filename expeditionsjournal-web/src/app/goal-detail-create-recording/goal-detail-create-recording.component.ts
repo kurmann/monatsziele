b434aef1313b521984b3b80ad8bf3839cf1b12a1
kurmann <kurmannwillisau@me.com>
2018-06-07 14:20:12 +0200

    Intialize component with getting goal ID from parent #16

--- expeditionsjournal-web/src/app/goal-detail-create-recording/goal-detail-create-recording.component.ts
+++ expeditionsjournal-web/src/app/goal-detail-create-recording/goal-detail-create-recording.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GoalService } from '../goal-service/goal.service';
+import { Goal } from '../goal';
 
 @Component({
   templateUrl: './goal-detail-create-recording.component.html',
@@ -6,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalDetailCreateRecordingComponent implements OnInit {
 
-  constructor() { }
+  _goal: Goal;
+
+  constructor(private _route: ActivatedRoute, private _goalService: GoalService) { }
 
   ngOnInit() {
+    const id = this._route.parent.snapshot.paramMap.get('id');
+    this._goal = this._goalService.getGoal(id);
+
+    console.log('Goal:', this._goal.id);
   }
 
 }
