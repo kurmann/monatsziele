c013c760fce9bb2cd84ca3fdd5a43dce80b57a59
Patrick Kurmann <kurmannwillisau@me.com>
2018-06-13 12:58:54 +0200

    Added table controller and Goals entity is now based on TableEntity

---
+++ Controllers/TablesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Monatsziele.Api.Controllers
+{
+    public class TablesController : ApiController
+    {
+        public IHttpActionResult CreateTable()
+        {
+            return View();
+        }
+    }
+}
--- Models/Goal.cs
+++ Models/Goal.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Monatsziele.Api.Models
 {
-    public class Goal
+    public class Goal : TableEntity
     {
-        public Guid Id { get; set; }
+        public Goal(string username, string id) : base(username, id)
+        {
+            PartitionKey = username;
+            RowKey = id;
+        }
 
         public string Name { get; set; }
     }
