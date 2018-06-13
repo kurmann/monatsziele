d17bb5d7deca711ddac49baa79ef83d8f65a9e13
Patrick Kurmann <kurmannwillisau@me.com>
2018-06-13 13:14:37 +0200

    Get cloud storage acount

--- Controllers/TablesController.cs
+++ Controllers/TablesController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Monatsziele.Api.Controllers
 {
     public class TablesController : Controller
     {
+        public CloudTableClient TableClient { get; set; }
+
         public void CreateTable()
         {
+            // table storage client
+            var tableCloudStorageAccount = CloudStorageAccount.Parse("monatsziele_AzureStorageConnectionString");
+            TableClient = tableCloudStorageAccount.CreateCloudTableClient();
+
+            // get table reference
+            var goalTable = TableClient.GetTableReference("Goals");
             
         }
     }
