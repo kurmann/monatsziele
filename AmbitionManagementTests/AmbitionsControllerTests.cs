86d5c7cf5b67157dea8119d9d4b2f5bec6ece2a7
kurmann <kurmannwillisau@me.com>
2018-10-08 20:57:02 +0200

    Created unit test project. Note: Intellisense on referenced projects

---
+++ AmbitionManagementTests/AmbitionsControllerTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using AmbitionManagement.Controllers;
+
+namespace AmbitionManagementTests
+{
+    public class AmbitionsControllerTests
+    {
+        [Fact]
+        public void Post_Test()
+        {
+            // arrange
+            var ambitionsController = new AmbitionsController();
+
+            // act
+            
+        }
+    }
+}
